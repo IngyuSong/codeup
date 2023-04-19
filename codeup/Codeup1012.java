b8ad264e775d977cc0aca0bb6149bf52221257b7
Ingyu Song <ingyu3879@gmail.com>
2023-04-19 16:39:36 +0900

    feat: Codeup1012

---
+++ codeup/Codeup1012.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(String.format("%6f",sc.nextFloat()));
+    }
+}
