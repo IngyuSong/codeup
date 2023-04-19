4fe073263d26776c6a8db8160ca2b5e3fa410dee
Ingyu Song <ingyu3879@gmail.com>
2023-04-19 16:52:35 +0900

    feat: Codeup1017

---
+++ codeup/Codeup1017.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=0; i<3; i++){
+            System.out.print(a + " ");
+        }
+    }
+}
