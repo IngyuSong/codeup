0af295d3c5b8fcb85fa4d561553fb06b5bb89f11
Ingyu Song <ingyu3879@gmail.com>
2023-04-19 16:11:06 +0900

    feat: Codeup1006

--- codeup/Codeup1002.java
+++ codeup/Codeup1002.java
@@ -1,3 +1,5 @@
+package codeup;
+
 public class Codeup1002 {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- codeup/Codeup1003.java
+++ codeup/Codeup1003.java
@@ -1,3 +1,5 @@
+package codeup;
+
 public class Codeup1003 {
     public static void main(String[] args) {
         System.out.println("Hello\nWorld");
