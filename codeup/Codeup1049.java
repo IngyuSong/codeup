a0d8888eb7753dfbb2f654107b2a410adfcfb124
Ingyu Song <ingyu3879@gmail.com>
2023-04-20 16:59:09 +0900

    feat: Codeup1049

--- codeup/Codeup1049.java
+++ codeup/Codeup1049.java
@@ -2,9 +2,9 @@ package codeup;
 
 import java.util.Scanner;
 
-public class Codeup1048 {
+public class Codeup1049 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() << sc.nextInt());
+        System.out.println(sc.nextInt() > sc.nextInt() ? 1 : 0);
     }
 }
